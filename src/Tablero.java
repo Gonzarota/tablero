7bdaae2487dd55878c8036cc69e01c28728aeb64
Gonzarota <gonzape93@gmail.com>
2020-07-06 14:10:02 -0400

    tablero 3 formas

--- src/Tablero.java
+++ src/Tablero.java
@@ -4,7 +4,7 @@ public class Tablero {
     public static void main(String[] args) {
         Scanner input= new Scanner(System.in);
         System.out.println("De que tamaño quieres el tablero");
-        int dimension= input.nextInt();
+        /*int dimension= input.nextInt();
         for(int i=0; i<dimension;i++){
             if(i%2==0){
                 for(int j=0; j<dimension;j++){
@@ -25,6 +25,33 @@ public class Tablero {
                 }
                 System.out.println();
             }
+        }/*
+
+
+
+        /*int dimension = input.nextInt();
+        for(int i = 0; i < dimension; i++){
+            for(int j = 0; j < dimension; j++){
+                if((i % 2 == 0 && j % 2 == 0)||(i % 2 != 0 && j % 2 != 0)){
+                    System.out.print("#  ");
+                }else{
+                    System.out.print("-  ");
+                }
+            }
+            System.out.println();
+        }*/
+
+
+        int dimension = input.nextInt();
+        for(int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                if ((i+j) % 2 == 0) {
+                    System.out.print("#  ");
+                }else {
+                    System.out.print("-  ");
+                }
+            }
+            System.out.println();
         }
     }
 }
